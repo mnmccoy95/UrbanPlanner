5975ff054303a72b072e2df0649d7fc4b3772f82
mnmccoy95 <mnmccoy95@gmail.com>
2020-11-17 11:19:00 -0600

    Building class set up

---
+++ Building.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace Planner
+{
+    public class Building
+    {
+        private string _designer = "Morrigan McCoy";
+        private DateTime _dateConstructed;
+        private string _address;
+        private string _owner;
+        public int Stories {get; set;}
+        public double Width {get; set;}
+        public double Depth {get; set;}
+        public double Volume
+        {
+            get
+            {
+                return Width*Depth*(3*Stories);
+            }
+        }
+
+        public Building(string address) {
+            _address = address;
+        }
+
+        public void Construct() {
+            _dateConstructed = DateTime.Now;
+        }
+
+        public void Purchase(string person) {
+            _owner = person;
+        }
+    }
+}
